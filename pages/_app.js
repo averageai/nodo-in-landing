34cbc81227bdcb43ee83f9fae76807327359e1bd
David Godoy <weare.average.ai@gmail.com>
2025-08-13 00:12:35 -0500

    fix: force dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    // Forzar modo oscuro
+    document.documentElement.classList.add('dark');
+    document.documentElement.style.colorScheme = 'dark';
+  }, []);
+
   return <>
     {/* Forzar a Tailwind a incluir animate-fade-in */}
     <div className="hidden animate-fade-in"></div>
